dd401490a9537ea7eaa76b56c6d808b9a825dfc0
Jesse Ruth <madongo@gmail.com>
2018-12-23 07:00:43 -0800

    Update ghp

---
+++ build/precache-manifest.5382ea01346aa4e226c0a738488c7c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EOSReact/build/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a0fb1bc11facb0a4a2b",
+    "url": "/EOSReact/build/index.html/static/js/runtime~main.2a0fb1bc.js"
+  },
+  {
+    "revision": "c791398732b96689f916",
+    "url": "/EOSReact/build/index.html/static/js/main.c7913987.chunk.js"
+  },
+  {
+    "revision": "4d42615d96a008136b6b",
+    "url": "/EOSReact/build/index.html/static/js/1.4d42615d.chunk.js"
+  },
+  {
+    "revision": "c791398732b96689f916",
+    "url": "/EOSReact/build/index.html/static/css/main.cde2cb44.chunk.css"
+  },
+  {
+    "revision": "8773c263db99d5151c104ba192a8265d",
+    "url": "/EOSReact/build/index.html/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93f2a6ee122792b05783a2dff0097e97.js"
+  "/EOSReact/build/index.html/precache-manifest.5382ea01346aa4e226c0a738488c7c49.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/EOSReact/build/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
